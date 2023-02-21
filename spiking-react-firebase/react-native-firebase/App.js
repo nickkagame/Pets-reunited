73ce557d7c2d4ff1249f702fea83ad29c7eb87b8
RonnieOne <lu.kee@hotmail.co.uk>
2023-02-21 12:31:28 +0000

    navigation stuff done

--- spiking-react-firebase/react-native-firebase/App.js
+++ spiking-react-firebase/react-native-firebase/App.js
@@ -1,28 +1,55 @@
-import 'react-native-gesture-handler';
-import React, { useEffect, useState } from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
-import {decode, encode} from 'base-64'
-if (!global.btoa) {  global.btoa = encode }
-if (!global.atob) { global.atob = decode }
+import "react-native-gesture-handler";
+import React, { useEffect, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { LoginScreen, HomeScreen, RegistrationScreen } from "./src/screens";
+import { firebase } from "./src/firebase/config"; //
+import { decode, encode } from "base-64";
+import { Text } from "react-native"; //
+if (!global.btoa) {
+  global.btoa = encode;
+}
+if (!global.atob) {
+  global.atob = decode;
+}
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [loading, setLoading] = useState(null);
+  const [user, setUser] = useState(null);
 
-  
-  const [loading, setLoading] = useState(true)
-  const [user, setUser] = useState(null)
-
-  
+  if (loading) {
+    return <Text>loading ...</Text>;
+  }
+  useEffect(() => {
+    const usersRef = firebase.firestore().collection("users");
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        usersRef
+          .doc(user.uid)
+          .get()
+          .then((document) => {
+            const userData = document.data();
+            setLoading(false);
+            setUser(userData);
+          })
+          .catch((error) => {
+            setLoading(false);
+          });
+      } else {
+        setLoading(false);
+      }
+    });
+  }, []);
 
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        { user ? (
+        {user ? (
           <Stack.Screen name="Home">
-            {props => <HomeScreen {...props} extraData={user} />}
+            {(props) => <HomeScreen {...props} extraData={user} />}
+            {/* <SinglePet /> */}
           </Stack.Screen>
         ) : (
           <>
