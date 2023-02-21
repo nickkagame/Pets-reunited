4772490fd97d24e211981509bd47b2625b04c6aa
Catherine Li <manheicatherine@gmail.com>
2023-02-21 13:42:05 +0000

    completed setup

--- spiking-react-firebase/react-native-firebase/App.js
+++ spiking-react-firebase/react-native-firebase/App.js
@@ -28,6 +28,7 @@ export default function App() {
           <>
             <Stack.Screen name="Login" component={LoginScreen} />
             <Stack.Screen name="Registration" component={RegistrationScreen} />
+            
           </>
         )}
       </Stack.Navigator>
