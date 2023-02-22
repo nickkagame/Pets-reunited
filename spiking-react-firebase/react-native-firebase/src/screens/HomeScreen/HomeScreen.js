f0381f671e0dc0ad6f591db096ef8db641026adf
Marek <mark.andrew222@yahoo.com>
2023-02-22 15:56:49 +0000

    rendering the list with imgs

--- spiking-react-firebase/react-native-firebase/src/screens/HomeScreen/HomeScreen.js
+++ spiking-react-firebase/react-native-firebase/src/screens/HomeScreen/HomeScreen.js
@@ -3,7 +3,8 @@ import { Pressable, SafeAreaView, Text, View, Image } from "react-native";
 import { collection, getDocs, QuerySnapshot } from "@firebase/firestore";
 import firebase from "firebase/compat";
 import { ScrollView } from "react-native-gesture-handler";
-import storage from "@react-native-firebase/storage"; //
+// import storage from "@react-native-firebase/storage"; //
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 const db = firebase.firestore();
 
@@ -12,39 +13,59 @@ export default function HomeScreen({ props }) {
   const [imageUrl, setImageUrl] = useState(null); //
 
   const getPets = async () => {
+    const storage = getStorage();
     const queryPets = await db.collection("lost_pets").get();
     const newPets = [];
+    const newURL = []; //
     queryPets.forEach((doc) => {
       const pet = { ...doc.data(), id: doc.id };
       newPets.push(pet);
+      newURL.push(pet.picture); //
+      // console.log(newURL);
     });
+
+    // something.forEach((imgname) => {
+    //   const reference
+    // })
     // console.log(pets, " <<<<<<<<<< PETS VARIABLE");
     setPets(newPets);
+
+    const storageRef = async () => {
+      let newPets2 = [];
+      pets.forEach((pet) => {
+        newPets2 = { ...pet, urlRef: ref(storage, pets.picture) };
+        console.log(newPets2);
+      });
+
+      // await getDownloadURL(pet.urlRef);
+      // setPets(newPets2);
+      // console.log(pets2);
+
+      const reference = ref(storage, pets[0].picture);
+
+      await getDownloadURL(reference).then((pic) => {
+        setImageUrl(pic);
+      });
+      // console.log(pets[1].picture);
+      // console.log(pets);
+    };
+    storageRef();
   };
 
   useEffect(() => {
     getPets();
-  }, []);
+  }, [imageUrl]);
 
-  const baseStorageUrl =
-    "https://firebasestorage.googleapis.com/v0/b/pets-reunited.appspot.com/o/";
-
-  // const newURL = baseStorageUrl + pets[0].picture;
-  const newURL = baseStorageUrl;
-  // console.log(pets[1].picture.slice(31), "PETS URL <<<<<<<<<<<<<<<<");
-  // console.log(baseStorageUrl, "BASE URL <<<<<<<<<<<<<<<<");
-  // console.log(newURL, "NEW URL <<<<<<<<<<<<<<<<");
   return (
     <ScrollView>
       <View>
         {pets.map((pet) => {
-          //   let img = pet.picture;
           return (
             <ScrollView key={pet.id}>
               <Text>{pet.your_name}</Text>
               <Image
                 source={{
-                  uri: baseStorageUrl + encodeURIComponent(pet.picture),
+                  uri: pet.picture,
                 }}
                 style={{ width: 200, height: 200 }}
               />
