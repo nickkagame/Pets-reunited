c83db0ac95ac9bf69a8a755be02afdb321e15189
Catherine <manheicatherine@gmail.com>
2023-02-23 15:33:47 +0000

    working on search component

--- spiking-react-firebase/react-native-firebase/src/screens/Search/Search.js
+++ spiking-react-firebase/react-native-firebase/src/screens/Search/Search.js
@@ -8,9 +8,12 @@ import {
   TouchableOpacity,
   StyleSheet,
   Button,
+  TextInput
 } from "react-native";
 import firebase from "firebase/compat";
 import { useNavigation } from "@react-navigation/native";
+import Footer from "../Footer/Footer";
+import SelectDropdown from 'react-native-select-dropdown'
 
 const db = firebase.firestore();
 
@@ -35,10 +38,31 @@ export default function HomeScreen({ props }) {
   };
 
   return (
-    <View style={styles.footer}>
-      <Button title="Inbox"></Button>
-      <Button title="Search" onPress={() => goToSearch()}></Button>
-      <Button title="User Profile"></Button>
+    <View >
+        <TextInput
+        editable
+        multiline
+        numberOfLines={4}
+        maxLength={40}
+             style={{padding: 10}}
+      />
+     <Footer />
+     <SelectDropdown
+	data={countries}
+	onSelect={(selectedItem, index) => {
+		console.log(selectedItem, index)
+	}}
+	buttonTextAfterSelection={(selectedItem, index) => {
+		// text represented after item is selected
+		// if data array is an array of objects then return selectedItem.property to render after item is selected
+		return selectedItem
+	}}
+	rowTextForSelection={(item, index) => {
+		// text represented for each item in dropdown
+		// if data array is an array of objects then return item.property to represent item in dropdown
+		return item
+	}}
+/>
     </View>
   );
 }
