ddebd9ba693dfbfa08185091bc347124f015ea95
Catherine <manheicatherine@gmail.com>
2023-02-28 16:54:02 +0000

    refactor pickImage and uploadPic function and fix the calendar closing after selected the date and styling the select drop down

--- spiking-react-firebase/react-native-firebase/src/screens/Calendar.js/Calendar.js
+++ spiking-react-firebase/react-native-firebase/src/screens/Calendar.js/Calendar.js
@@ -1,5 +1,9 @@
 import CalendarPicker from "react-native-calendar-picker";
 
-export default function CalendarPopUp({ setSelectedStartDate }) {
-  return <CalendarPicker onDateChange={setSelectedStartDate} />;
+export default function CalendarPopUp({ setSelectedStartDate, setIsClicked }) {
+  const handleSubmit =(e)=>{
+    setSelectedStartDate(e)
+    setIsClicked(false)
+  }
+  return <CalendarPicker onDateChange={(e)=>{handleSubmit(e)}} />;
 }
--- spiking-react-firebase/react-native-firebase/src/screens/PostPet/PostPet.js
+++ spiking-react-firebase/react-native-firebase/src/screens/PostPet/PostPet.js
@@ -51,19 +51,48 @@ export default function PostPet({ extraData }) {
   const [selectedStartDate, setSelectedStartDate] = useState("");
   const [isClicked, setIsClicked] = useState(false);
   const [location, setLocation] = useState("");
-  const [coordinates, setCoordinates] = useState({})
-  const [town, setTown] = useState('')
-  const [postcode, setPostcode] = useState('')
-  const [imageIsPicked, setImageIsPicked] = useState(false)
+  const [coordinates, setCoordinates] = useState({});
+  const [town, setTown] = useState("");
+  const [postcode, setPostcode] = useState("");
+  const [imageIsPicked, setImageIsPicked] = useState(false);
 
   const dayjs = require("dayjs");
   const date = dayjs(selectedStartDate).format("MMMM DD YYYY");
 
-  const uploadImage = async () => {
-    const blob = await new Promise((resolve, reject) => {
+  const uploadImage = () => {
+    return new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
       xhr.onload = function () {
-        resolve(xhr.response);
+        const blob = xhr.response;
+        const photoFileId = uuid.v4();
+        const ref = firebase.storage().ref().child(`Pictures/${photoFileId}`);
+        const snapshot = ref.put(blob);
+  
+        snapshot.on(
+          firebase.storage.TaskEvent.STATE_CHANGED,
+          () => {
+            // Uploading state
+            setUploading(true);
+          },
+          (error) => {
+            // Error state
+            setUploading(false);
+            console.log(error);
+            blob.close();
+            reject(new TypeError("Network request failed"));
+          },
+          () => {
+            // Success state
+            snapshot.snapshot.ref.getDownloadURL().then((url) => {
+              setUploading(false);
+              console.log("Download URL: ", url);
+              alert("image successfully uploaded");
+              setImage(url);
+              blob.close();
+              resolve(url);
+            });
+          }
+        );
       };
       xhr.onerror = function () {
         reject(new TypeError("Network request failed"));
@@ -72,55 +101,41 @@ export default function PostPet({ extraData }) {
       xhr.open("GET", image, true);
       xhr.send(null);
     });
-    const photoFileId = uuid.v4();
-    const ref = firebase.storage().ref().child(`Pictures/${photoFileId}`);
-    const snapshot = ref.put(blob);
-    snapshot.on(
-      firebase.storage.TaskEvent.STATE_CHANGED,
-      () => {
-        setUploading(true);
-      },
-      (error) => {
-        setUploading(false);
-        console.log(error);
-        blob.close();
-        return;
-      },
-      () => {
-        snapshot.snapshot.ref.getDownloadURL().then((url) => {
-          setUploading(false);
-          console.log("Download URL: ", url);
-          alert("image successfully uploaded")
-          setImage(url);
-          blob.close();
-          return url;
-        });
-      }
-    );
   };
+  
 
-  const handlePetTypeSelection = () => {
-
-  }
-
-  const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
+  const pickImage = () => {
+    ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
+    })
+    .then((result) => {
+      if (!result.canceled) {
+        setImageIsPicked(true);
+        setImage(result.assets[0].uri);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
     });
-    if (!result.canceled) {
-      setImageIsPicked(true)
-      setImage(result.assets[0].uri);
-    }
   };
+  
 
   const handleSubmit = async () => {
-  if(pet_name.length === 0 || email.length === 0 || location.length === 0 || pet_type.length === 0 || your_name.length === 0 || uploading === true || imageIsPicked === false){
-    alert("Please complete all required field, and upload an image")
-    return 
-  }
+    if (
+      pet_name.length === 0 ||
+      email.length === 0 ||
+      location.length === 0 ||
+      pet_type.length === 0 ||
+      your_name.length === 0 ||
+      uploading === true ||
+      imageIsPicked === false
+    ) {
+      alert("Please complete all required field, and upload an image");
+      return;
+    }
 
     try {
       const submitRef = await addDoc(collection(db, "lost_pets"), {
@@ -138,10 +153,10 @@ export default function PostPet({ extraData }) {
         userProfileName: extraData.fullName,
         postcode,
         coordinates,
-        town
+        town,
       });
-      setIsClicked(false); 
-      alert("Post successful!")
+      setIsClicked(false);
+      alert("Post successful!");
       navigation.navigate("Home");
     } catch (e) {
       console.error(e);
@@ -152,7 +167,6 @@ export default function PostPet({ extraData }) {
 
   return (
     <>
-
       <ScrollView
         keyboardShouldPersistTaps={"handled"}
         horizontal={false}
@@ -196,30 +210,30 @@ export default function PostPet({ extraData }) {
           horizontal={true}
           style={styles.inputAuto}
         >
-          <AutoComp setLocation={setLocation} setCoordinates ={setCoordinates} setTown = {setTown} 
-  setPostcode = {setPostcode}/>
+          <AutoComp
+            setLocation={setLocation}
+            setCoordinates={setCoordinates}
+            setTown={setTown}
+            setPostcode={setPostcode}
+          />
         </ScrollView>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter chip id (optional)"
-          value={chipId}
-          onChangeText={(e) => {
-            setChipId(e);
+      
+        <SelectDropdown
+        defaultButtonText={'Select Pet Type'}
+          style={styles.selectinput}
+          data={petTypes}
+          buttonStyle={styles.selectinput}
+        
+          onSelect={(selectedItem, index) => {
+            setPet_type(selectedItem);
+          }}
+          buttonTextAfterSelection={(selectedItem, index) => {
+            return selectedItem;
+          }}
+          rowTextForSelection={(item, index) => {
+            return item;
           }}
         />
-        <SelectDropdown
-            style={styles.input}
-            data={petTypes}
-            onSelect={(selectedItem, index) => {
-              setPet_type(selectedItem);
-            }}
-            buttonTextAfterSelection={(selectedItem, index) => {
-              return selectedItem;
-            }}
-            rowTextForSelection={(item, index) => {
-              return item;
-            }}
-          />
         <TextInput
           style={styles.input}
           placeholder="More details of lost pet (required)"
@@ -227,8 +241,16 @@ export default function PostPet({ extraData }) {
           onChangeText={(e) => {
             setDescription(e);
           }}
+        /> 
+         <TextInput
+          style={styles.input}
+          placeholder="Enter chip id (optional)"
+          value={chipId}
+          onChangeText={(e) => {
+            setChipId(e);
+          }}
         />
-        <Text style={styles.datePicked}>
+        <Text  style={styles.inputDate}>
           {date.toString() === "Invalid Date"
             ? "Please pick a date"
             : date.toString()}
@@ -241,7 +263,7 @@ export default function PostPet({ extraData }) {
           }}
         >
           {isClicked ? (
-            <CalendarPopUp setSelectedStartDate={setSelectedStartDate} />
+            <CalendarPopUp setSelectedStartDate={setSelectedStartDate} setIsClicked={setIsClicked}/>
           ) : (
             ""
           )}
@@ -252,9 +274,16 @@ export default function PostPet({ extraData }) {
           <Text style={styles.buttonText}>Choose pic</Text>
         </TouchableOpacity>
 
-        {!uploading ? <TouchableOpacity style={styles.buttonContainer} onPress={uploadImage}>
-          <Text style={styles.buttonText}>Upload Image</Text>
-        </TouchableOpacity>:  <ActivityIndicator size={'small'} color='black' />}
+        {!uploading ? (
+          <TouchableOpacity
+            style={styles.buttonContainer}
+            onPress={uploadImage}
+          >
+            <Text style={styles.buttonText}>Upload Image</Text>
+          </TouchableOpacity>
+        ) : (
+          <ActivityIndicator size={"small"} color="black" />
+        )}
 
         <TouchableOpacity
           style={styles.buttonContainerBottom}
@@ -292,17 +321,49 @@ const styles = StyleSheet.create({
     marginLeft: 30,
     marginRight: 30,
     paddingLeft: 16,
+    width: 350
   },
-  inputAuto: {
-    height: "auto",
+  selectinput: {
+    height: 48,
     borderRadius: 5,
-    // overflow: "visible",
+    overflow: "hidden",
     backgroundColor: "white",
     marginTop: 6,
     marginBottom: 6,
     marginLeft: 30,
     marginRight: 30,
     paddingLeft: 16,
+    width: 350,
+   
+  },
+  inputAuto: {
+     height: 48,
+    borderRadius: 5,
+    overflow: "hidden",
+    backgroundColor: "white",
+    marginTop: 6,
+    marginBottom: 6,
+    marginLeft: 30,
+    marginRight: 30,
+    paddingTop: 2,
+    paddingLeft: 8,
+    width: 350,
+  },
+  inputDate:{
+    height: 48,
+    borderRadius: 5,
+    overflow: "hidden",
+    backgroundColor: "white",
+    marginTop: 6,
+    marginBottom: 6,
+    marginLeft: 30,
+    marginRight: 30,
+    paddingLeft: 16,
+    paddingTop: 15,
+    width: 350,
+    fontSize:14,
+    color:"silver"
+   
   },
   buttonContainer: {
     marginRight: 7,
@@ -345,12 +406,7 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     textTransform: "uppercase",
   },
-  datePicked: {
-    alignSelf: "center",
-    fontWeight: "bold",
-    marginTop: 5,
-    marginBottom: 5,
-  },
+  
   calendarContainer: {
     marginRight: 7,
     marginLeft: 7,
