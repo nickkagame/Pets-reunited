380b600135b694a2e3132e97b553f4e79daf3812
Catherine <manheicatherine@gmail.com>
2023-03-01 17:13:14 +0000

    updated single pet page styling

--- spiking-react-firebase/react-native-firebase/src/screens/PetSingle/PetSingle.js
+++ spiking-react-firebase/react-native-firebase/src/screens/PetSingle/PetSingle.js
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   ScrollView,
   TouchableOpacity,
+  View,
 } from "react-native";
 import Footer from "../Footer/Footer";
 
@@ -14,29 +15,30 @@ export default function PetSingle({ route, extraData }) {
   // console.log(pet);
   // console.log(route.params.pets);
   const sendEmail = () => {
-    Linking.openURL(`mailto:${pet.email}?subject=Regarding ${pet.pet_name}`).catch((err)=> {
-      console.log(err)
-      alert("Local email application not setup!")
-      return
+    Linking.openURL(
+      `mailto:${pet.email}?subject=Regarding ${pet.pet_name}`
+    ).catch((err) => {
+      console.log(err);
+      alert("Local email application not setup!");
+      return;
     });
-  }
-  const dateString = pet.lastSeenDate.split(' ').slice(0, 4).join(' ');
-
+  };
+  const dateString = pet.lastSeenDate.split(" ").slice(0, 4).join(" ");
 
   return (
     <>
       <ScrollView style={styles.container}>
         <Text style={styles.title}>{pet.pet_name}</Text>
         <Image source={{ uri: pet.picture }} style={styles.image} />
-        <Text style={styles.heading}>Hello! My name is {pet.pet_name}.</Text>
-               <Text style={styles.text}>
-          I was last seen on {dateString}. 
-        </Text>
-        <Text style={styles.text}>
-          My last known location was {pet.location} :(.
-        </Text>
-        <Text style={styles.text}>I really miss my owner {pet.your_name}.</Text>
-        <Text style={styles.text}>More details : {pet.description}.</Text><TouchableOpacity style={styles.buttonContainer} onPress={sendEmail}>
+        <View style={styles.petDetailsContainer}>
+          <Text style={styles.petDetails}>
+            Hello! My name is {pet.pet_name}. I was last seen on {dateString}.
+            My last known location was {pet.location} :(. I really miss my owner{" "}
+            {pet.your_name}.
+          </Text>
+          <Text style={styles.petDetails}>More details : {pet.description}.</Text>
+        </View>
+        <TouchableOpacity style={styles.buttonContainer} onPress={sendEmail}>
           <Text style={styles.buttonText}>Contact owner</Text>
         </TouchableOpacity>
       </ScrollView>
@@ -48,7 +50,8 @@ export default function PetSingle({ route, extraData }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#5cc8d7",
+    backgroundColor: "#cff7fc",
+    padding: 20,
   },
   title: {
     fontSize: 26,
@@ -58,15 +61,25 @@ const styles = StyleSheet.create({
     paddingHorizontal: 32,
     alignSelf: "center",
     fontWeight: "bold",
+    textShadowColor: "#4769fc",
+    textShadowRadius: 10,
+    
   },
-
-  heading: {
+  petDetailsContainer: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    margin: 10,
+    paddingVertical: 12,
+  },
+  petDetails: {
     fontSize: 20,
     color: "#000",
     justifyContent: "center",
     paddingVertical: 12,
     paddingHorizontal: 32,
     alignSelf: "center",
+    fontSize:18,
+    lineHeight: 24,
   },
 
   text: {
@@ -74,6 +87,8 @@ const styles = StyleSheet.create({
     alignContent: "center",
     paddingVertical: 10,
     alignSelf: "center",
+    lineHeight: 2,
+    fontSize:18
   },
 
   image: {
@@ -96,7 +111,7 @@ const styles = StyleSheet.create({
     margin: 20,
     shadowRadius: 1.5,
     shadowOpacity: 0.5,
-    shadowColor: "black",
+    shadowColor: "#0d2175",
   },
 
   buttonText: {
