9bd8e07bcff59b2fed3c10e8f188c0cafc72f739
YoriB <deyoribad@yahoo.co.uk>
2023-03-01 17:18:17 +0000

    footer-under-dev

--- spiking-react-firebase/react-native-firebase/src/screens/Footer/Footer.js
+++ spiking-react-firebase/react-native-firebase/src/screens/Footer/Footer.js
@@ -8,6 +8,7 @@ import {
   TouchableOpacity,
   StyleSheet,
   Button,
+  TouchableWithoutFeedback,
 } from "react-native";
 import firebase from "firebase/compat";
 import { useNavigation } from "@react-navigation/native";
@@ -40,29 +41,26 @@ export default function HomeScreen({ props, pets, pet }) {
 
   return (
     <View style={styles.container}>
-      <View style={styles.footer}>
-        <TouchableOpacity style={styles.iconContainer}>
-          <Icon name="envelope-o" size={35} color="#900" />
-        </TouchableOpacity>
+      <View style={styles.footer}>      
 
         <TouchableOpacity
           style={styles.iconContainer}
           onPress={() => goToSearch()}
         >
-          <Icon name="search" size={35} color="#900" />
+          <Icon name="search" size={40} color="#000" />
         </TouchableOpacity>
 
         <TouchableOpacity
           style={styles.iconContainer}
           onPress={() => goToUserProfile()}
         >
-          <Icon name="user-circle-o" size={35} color="#900" />
+          <Icon name="user-circle-o" size={40} color="#000" />
         </TouchableOpacity>
         <TouchableOpacity
           style={styles.iconContainer}
           onPress={() => goToMapPage()}
         >
-          <Icon name="map-marker" size={35} color="#900" />
+          <Icon name="map-marker" size={40} color="#000" />
         </TouchableOpacity>
       </View>
     </View>
@@ -73,19 +71,37 @@ const styles = StyleSheet.create({
   container: {
     flexend: 0,
     backgroundColor: "#5cc8d7",
+    
   },
-  footer: {
-    backgroundColor: "yellow",
-    padding: 30,
+  footer: {    
+     bottom : 25,    
+     elevation : 0,
+    // backgroundColor : '~ffffff', 
+    backgroundColor: "orange",
+    padding: 20,
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center",
     justifyContent: "space-evenly",
-    borderRadius: 50,
+    // ...styles.shadow,   
   },
   iconContainer: {
+    flex : 1,
     justifyContent: "center",
     alignItems: "center",
-    justifyContent: "flex-end",
+    justifyContent: "space-evenly",
   },
 });
+
+// const styles1 = StyleSheet.create({
+//   shadow: {
+//     shadowColor: "black",
+//     shadowOffset: {
+//       width : 0,
+//       height : 10,
+//   },
+//   shadowOpacity : 0.25,
+//   shadowRadius :10,
+//   elevation : 5,
+//   }
+// })
